1ea8f2a0cc94e452e5434ac6359964d1b6a312a1
PersianKid100 <shayan_asadpour@hotmail.com>
2025-03-25 00:44:43 -0400

    nofooter

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center p-4 bg-gray-800 text-sm text-gray-400">
-      © {new Date().getFullYear()} Your Name. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
